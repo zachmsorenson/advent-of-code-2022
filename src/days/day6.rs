a9246cb2bb525e15eb8034eee553c85a4539e9c3
Zach Sorenson <zachary.sorenson@nextsilicon.com>
2022-12-06 09:16:51 -0600

    Clean up

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -2,60 +2,26 @@ use std::collections::HashMap;
 use std::collections::VecDeque;
 
 
-/// Parse each line to an integer.
 pub fn generator(input: &str) -> Vec<char> {
     input.chars().collect()
 }
 
-// /// Implemented with a VecDeque
-// pub fn part1(input: &[char]) -> u32 {
-//     let mut deque = VecDeque::with_capacity(5);
-//     for i in 0..input.len() {
-//         let next = input[i];
-//         deque.push_back(next);
-
-//         if i >= 3 {
-//             let (a, b, c, d) = (deque[0], deque[1], deque[2], next);
-//             if a != b && a != c && a != d && b != c && b != d && c != d {
-//                 return i as u32 + 1;
-//             }
-
-//             deque.pop_front().unwrap();
-//         }
-//     }
-//     0
-// }
-
-/// Implemented with a HashMap
+/// Implemented with a VecDeque
 pub fn part1(input: &[char]) -> u32 {
-    let mut hashmap: HashMap<char, u32> = HashMap::with_capacity(26);
-
+    let mut deque = VecDeque::with_capacity(5);
     for i in 0..input.len() {
         let next = input[i];
-        match hashmap.get_mut(&next) {
-            Some(v) => {
-                *v += 1;
-            },
-            None => {
-                hashmap.insert(next, 1);
-            }
-        }
+        deque.push_back(next);
 
         if i >= 3 {
-            if hashmap.values().all(|&v| v <= 1) {
+            let (a, b, c, d) = (deque[0], deque[1], deque[2], next);
+            if a != b && a != c && a != d && b != c && b != d && c != d {
                 return i as u32 + 1;
             }
 
-            let lru = input[i-3];
-            match hashmap.get_mut(&lru) {
-                Some(u) => {
-                    *u -= 1;
-                },
-                None => unreachable!()
-            }
+            deque.pop_front().unwrap();
         }
     }
-
     0
 }
 
@@ -63,16 +29,13 @@ pub fn part1(input: &[char]) -> u32 {
 pub fn part2(input: &[char]) -> u32 {
     let mut hashmap: HashMap<char, u32> = HashMap::with_capacity(26);
 
+    for c in 'a'..='z' {
+        hashmap.insert(c, 0);
+    }
+
     for i in 0..input.len() {
         let next = input[i];
-        match hashmap.get_mut(&next) {
-            Some(v) => {
-                *v += 1;
-            },
-            None => {
-                hashmap.insert(next, 1);
-            }
-        }
+        *hashmap.get_mut(&next).unwrap() += 1;
 
         if i >= 13 {
             if hashmap.values().all(|&v| v <= 1) {
@@ -80,14 +43,8 @@ pub fn part2(input: &[char]) -> u32 {
             }
 
             let lru = input[i-13];
-            match hashmap.get_mut(&lru) {
-                Some(u) => {
-                    *u -= 1;
-                },
-                None => unreachable!()
-            }
+            *hashmap.get_mut(&lru).unwrap() -= 1;
         }
     }
-
     0
 }
